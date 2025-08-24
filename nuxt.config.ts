3c89d41849b1f961a8c1df15356eb28b2fa947be
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-08-24 16:35:36 +0500

    change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true, // or false, depending on your setup
-  nitro: {
-    preset: 'vercel',
-  },
+  ssr: true,
   app: {
     baseURL: '/',
     head: {
